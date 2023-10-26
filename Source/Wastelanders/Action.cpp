b647a94c4804195f4a5ebb613518f23c8e55f084
r-k03 <rahul.kamath2512@gmail.com>
2023-10-26 01:00:35 -0700

    action class
    added action class

---
+++ Source/Wastelanders/Action.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Action.h"
+
+Action::Action()
+{
+public:
+	//Player player;
+	//Enemy enemy;
+	//Card card;
+}
+
+Action::~Action()
+{
+}
