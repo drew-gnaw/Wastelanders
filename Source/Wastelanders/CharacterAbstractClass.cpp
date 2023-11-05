4b93ecac38d0cbe8ec761c5841335bd0937c6257
anjx123 <114792511+anjx123@users.noreply.github.com>
2023-11-04 18:40:23 -0700

    Health added to an abstract character class
    Health added to an abstract character class

--- Source/Wastelanders/CharacterAbstractClass.cpp
+++ Source/Wastelanders/CharacterAbstractClass.cpp
@@ -2,13 +2,21 @@
 
 
 #include "CharacterAbstractClass.h"
+#include "Math/UnrealMathUtility.h"
+
 
 // Sets default values
 ACharacterAbstractClass::ACharacterAbstractClass()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	health = MAX_HEALTH;
 
+}
+
+void ACharacterAbstractClass::TakeDamage(int damage)
+{
+	health = FMath::Clamp(health - damage, 0, MAX_HEALTH);
 }
 
 // Called when the game starts or when spawned
--- Source/Wastelanders/CharacterAbstractClass.h
+++ Source/Wastelanders/CharacterAbstractClass.h
@@ -15,6 +15,8 @@ public:
 	// Sets default values for this actor's properties
 	ACharacterAbstractClass();
 
+	void TakeDamage(int damage);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -22,5 +24,11 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+private:
+	//Private Fields
+	int health;
+	int const MAX_HEALTH = 30;
+
 
 };
